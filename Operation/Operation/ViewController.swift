cbdd3c0605bd20560cc43cadc9a76c7e7c32486a
JOJUNHEE <eh7457303j@gmail.com>
2021-06-28 23:09:10 +0900

    기본적인 operationQueue 사용

--- Operation/Operation/ViewController.swift
+++ Operation/Operation/ViewController.swift
@@ -8,12 +8,82 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MainOperationQueue 생성 방법
+    let mainQueue: OperationQueue = OperationQueue.main
+    
+    // BackgroundOperationQueue 생성 방법
+    let backgroundQueue: OperationQueue = OperationQueue()
+    
+    // isCancelled를 직접 추가
+    var isCancelled: Bool = false
+    
+    
+    @IBAction func startOperation(_ sender: Any) {
+        isCancelled = false
+        
+        // Block으로 바로 operation을 추가하는 방법
+        
+        backgroundQueue.addOperation {
+            autoreleasepool {
+                for _ in 1 ... 20 {
+                    guard self.isCancelled == false else { return }
+                    
+                    print("🍎", terminator: "")
+                    Thread.sleep(forTimeInterval: 0.3)
+                }
+            }
+        }
+        
+        
+        // BlockOperation을 생성하는 방법
+        
+        let block = BlockOperation {
+            autoreleasepool {
+                for _ in 1 ... 20 {
+                    guard self.isCancelled == false else { return }
+                    
+                    print("🍏", terminator: "")
+                    Thread.sleep(forTimeInterval: 0.6)
+                }
+            }
+        }
+        
+        // 생성한 blockOperation을 추가하는 방법
+        backgroundQueue.addOperation(block)
+        
+        // 기존 blockOperation에 operation 추가하는 방법
+        block.addExecutionBlock {
+            autoreleasepool {
+                for _ in 1 ... 20 {
+                    guard self.isCancelled == false else { return }
+                    
+                    print("🍑", terminator: "")
+                    Thread.sleep(forTimeInterval: 0.9)
+                }
+            }
+        }
+        
+        // block이 끝나면 실행시킬 코드
+        block.completionBlock = {
+            print("Done")
+        }
+        
+    }
+    
+    @IBAction func stopOperation(_ sender: Any) {
+        isCancelled = true
+        backgroundQueue.cancelAllOperations()
+    }
+    
+    // MARK: ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        stopOperation(self)
+    }
 }
 
