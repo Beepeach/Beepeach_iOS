6b6aa08cbe8cbfb64097ef00cf017692a5347afe
Beepeach <eh7457303j@gmail.com>
2021-10-15 17:21:00 +0900

    MusicPlayer 기본화면 구성

---
+++ MusicPlayer/MusicPlayer/MusicPlayer/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by JunHeeJo on 10/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: IBOutlets
+    @IBOutlet var playPauseButton: UIButton!
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var progressSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
