5adfba230fb35cca8e3bfb3f06ecaaf2536bd2bd
Beepeach <eh7457303j@gmail.com>
2021-10-17 15:11:26 +0900

    Code로 오토레이아웃 만드는 방법 추가

--- MusicPlayer/MusicPlayer/MusicPlayer/ViewController.swift
+++ MusicPlayer/MusicPlayer/MusicPlayer/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-    // MARK: - Properties
+    // MARK: Properties
     var player: AVAudioPlayer!
     var timer: Timer!
     
@@ -41,24 +41,6 @@ class ViewController: UIViewController {
         self.player.currentTime = TimeInterval(sender.value)
     }
     
-    func initializePlayer() {
-        guard let soundAsset: NSDataAsset = NSDataAsset(name: "sound") else {
-            return
-        }
-        
-        do {
-            try self.player = AVAudioPlayer(data: soundAsset.data)
-            self.player.delegate = self
-        } catch let error as NSError {
-            print("플레이어 초기화 실패")
-            print("코드: \(error.code), 메세지: \(error.localizedDescription)")
-        }
-        
-        self.progressSlider.maximumValue = Float(self.player.duration)
-        self.progressSlider.minimumValue = 0
-        self.progressSlider.value = Float(self.player.currentTime)
-    }
-    
     func updateTimeLabelText(time: TimeInterval) {
         let minute: Int = Int(time / 60)
         let second: Int = Int(time.truncatingRemainder(dividingBy: 60))
@@ -87,9 +69,115 @@ class ViewController: UIViewController {
     // MARK: ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        playPauseButton.setImage(UIImage(named: "button_play"), for: .normal)
-        playPauseButton.setImage(UIImage(named: "button_pause"), for: .selected)
+        self.addViewsWithCode()
         self.initializePlayer()
+    }
+    
+    // MARK: CustomMethod
+    func addViewsWithCode() {
+        self.addPlayPauseButton()
+        self.addTimeLabel()
+        self.addProgressSlider()
+    }
+    
+    func addPlayPauseButton() {
+        let button: UIButton = UIButton(type: .custom)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(button)
+        
+        button.setImage(UIImage(named: "button_play"), for: .normal)
+        button.setImage(UIImage(named: "button_pause"), for: .selected)
+        
+        button.addTarget(self, action: #selector(self.touchUpPlayPauseButton(_:)), for: .touchUpInside)
+        
+        let centerX: NSLayoutConstraint
+        centerX = button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
+        
+        let centerY: NSLayoutConstraint
+        centerY = NSLayoutConstraint(item: button, attribute: .centerY, relatedBy: .equal, toItem: self.view, attribute: .centerY, multiplier: 0.8, constant: 0)
+        
+        let width: NSLayoutConstraint
+        width = button.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.5)
+        
+        let ratio: NSLayoutConstraint
+        ratio = button.heightAnchor.constraint(equalTo: button.widthAnchor, multiplier: 1)
+        
+        centerX.isActive = true
+        centerY.isActive = true
+        width.isActive = true
+        ratio.isActive = true
+        
+        self.playPauseButton = button
+    }
+    
+    func addTimeLabel() {
+        let timeLabel: UILabel = UILabel()
+        timeLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(timeLabel)
+        
+        timeLabel.textColor = .black
+        timeLabel.textAlignment = .center
+        timeLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        
+        let centerX: NSLayoutConstraint
+        centerX = timeLabel.centerXAnchor.constraint(equalTo: self.playPauseButton.centerXAnchor)
+        
+        let top: NSLayoutConstraint
+        top = timeLabel.topAnchor.constraint(equalTo: self.playPauseButton.bottomAnchor, constant: 8)
+        
+        centerX.isActive = true
+        top.isActive = true
+
+        self.timeLabel = timeLabel
+        self.updateTimeLabelText(time: 0)
+    }
+    
+    func addProgressSlider() {
+        let slider: UISlider = UISlider()
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.view.addSubview(slider)
+        
+        slider.minimumTrackTintColor = .red
+        
+        slider.addTarget(self, action: #selector(self.sliderValueChanged(_:)), for: .valueChanged)
+        
+        let safeAreaGuide: UILayoutGuide = self.view.safeAreaLayoutGuide
+        
+        let centerX: NSLayoutConstraint
+        centerX = slider.centerXAnchor.constraint(equalTo: self.timeLabel.centerXAnchor)
+        
+        let top: NSLayoutConstraint
+        top = slider.topAnchor.constraint(equalTo: self.timeLabel.bottomAnchor, constant: 8)
+        
+        let leading: NSLayoutConstraint
+        leading = slider.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: 16)
+        
+        let trailing: NSLayoutConstraint
+        trailing = slider.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -16)
+        
+        NSLayoutConstraint.activate([centerX, top, leading, trailing])
+        
+        self.progressSlider = slider
+    }
+    
+    func initializePlayer() {
+        guard let soundAsset: NSDataAsset = NSDataAsset(name: "sound") else {
+            return
+        }
+        
+        do {
+            try self.player = AVAudioPlayer(data: soundAsset.data)
+            self.player.delegate = self
+        } catch let error as NSError {
+            print("플레이어 초기화 실패")
+            print("코드: \(error.code), 메세지: \(error.localizedDescription)")
+        }
+        
+        self.progressSlider.maximumValue = Float(self.player.duration)
+        self.progressSlider.minimumValue = 0
+        self.progressSlider.value = Float(self.player.currentTime)
     }
 }
 
