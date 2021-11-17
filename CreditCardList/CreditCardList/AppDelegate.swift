a6bc7ec6c2229a36dc4e5cdc2f4a6f49f9ac2e94
Beepeach <eh7457303j@gmail.com>
2021-11-17 22:08:45 +0900

    FireSotre를 이용하여 데이터 읽기, 쓰기 연습 및 코드 정리

--- CreditCardList/CreditCardList/AppDelegate.swift
+++ CreditCardList/CreditCardList/AppDelegate.swift
@@ -7,17 +7,57 @@
 
 import UIKit
 import Firebase
+import FirebaseFirestoreSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
+        // Firebase 초기화
         FirebaseApp.configure()
         
+        // Batch Data
+        let db = Firestore.firestore()
+        batchCardDate(database: db)
+        
         return true
+    }
+    
+    private func batchCardDate(database: Firestore) {
+        database.collection("creditCardList").getDocuments { snapshot, _ in
+            guard snapshot?.isEmpty == true else { return }
+            let batch = database.batch()
+            
+            let card0Reference = database.collection("creditCardList").document("card0")
+            let card1Reference = database.collection("creditCardList").document("card1")
+            let card2Reference = database.collection("creditCardList").document("card2")
+            let card3Reference = database.collection("creditCardList").document("card3")
+            let card4Reference = database.collection("creditCardList").document("card4")
+            let card5Reference = database.collection("creditCardList").document("card5")
+            let card6Reference = database.collection("creditCardList").document("card6")
+            let card7Reference = database.collection("creditCardList").document("card7")
+            let card8Reference = database.collection("creditCardList").document("card8")
+            let card9Reference = database.collection("creditCardList").document("card9")
+            
+            do {
+                try batch.setData(from: CreditCardDummy.card0, forDocument: card0Reference)
+                try batch.setData(from: CreditCardDummy.card1, forDocument: card1Reference)
+                try batch.setData(from: CreditCardDummy.card2, forDocument: card2Reference)
+                try batch.setData(from: CreditCardDummy.card3, forDocument: card3Reference)
+                try batch.setData(from: CreditCardDummy.card4, forDocument: card4Reference)
+                try batch.setData(from: CreditCardDummy.card5, forDocument: card5Reference)
+                try batch.setData(from: CreditCardDummy.card6, forDocument: card6Reference)
+                try batch.setData(from: CreditCardDummy.card7, forDocument: card7Reference)
+                try batch.setData(from: CreditCardDummy.card8, forDocument: card8Reference)
+                try batch.setData(from: CreditCardDummy.card9, forDocument: card9Reference)
+            } catch let error {
+                print("ERROR writing card to Firestore \(error.localizedDescription)")
+            }
+            
+            batch.commit()
+        }
     }
 
     // MARK: UISceneSession Lifecycle
--- CreditCardList/CreditCardList/CardListTableViewController.swift
+++ CreditCardList/CreditCardList/CardListTableViewController.swift
@@ -8,11 +8,13 @@
 import UIKit
 import Kingfisher
 import FirebaseDatabase
+import FirebaseFirestore
 
 class CardListTableViewController: UITableViewController {
     // MARK: Properties
     var creditCardList: [CreditCard] = []
-    var ref: DatabaseReference?
+    var reference: DatabaseReference?
+    var firestoreDB = Firestore.firestore()
     
     // MARK: ViewLifeCycle
     override func viewDidLoad() {
@@ -21,8 +23,39 @@ class CardListTableViewController: UITableViewController {
         let nibName = UINib(nibName: "CardListTableViewCell", bundle: nil)
         tableView.register(nibName, forCellReuseIdentifier: "CardListTableViewCell")
         
-        ref = Database.database().reference()
-        ref?.observe(.value) { snapshot in
+        fetchDataFrom(database: firestoreDB)
+        // fetchDateFromReference()
+    }
+    
+    // Firestore에서 데이터 읽어오기
+    private func fetchDataFrom(database: Firestore) {
+        database.collection("creditCardList").addSnapshotListener { snapshot, error in
+            guard let documents = snapshot?.documents else {
+                print("ERROR Firestore fetching document \(error?.localizedDescription as Any)")
+                return
+            }
+            
+            self.creditCardList = documents.compactMap { doc -> CreditCard? in
+                do {
+                    let jsonData = try JSONSerialization.data(withJSONObject: doc.data(), options: [])
+                    let creditCard = try JSONDecoder().decode(CreditCard.self, from: jsonData)
+                    return creditCard
+                } catch let error {
+                    print("ERROR JSON Parsing \(error.localizedDescription)")
+                    return nil
+                }
+            }.sorted { $0.rank < $1.rank }
+            
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    // RealTimeDB에서 데이터 읽어오기
+    private func fetchDateFromReference() {
+        reference = Database.database().reference()
+        reference?.observe(.value) { snapshot in
             guard let value = snapshot.value as? [String: [String: Any]] else { return
             }
             
@@ -40,10 +73,10 @@ class CardListTableViewController: UITableViewController {
             }
         }
     }
-
-    // MARK: - UITableViewDelegate, UITableView Datasource
+    
+    
+    // MARK: - UITableView Datasource
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return creditCardList.count
     }
     
@@ -62,6 +95,9 @@ class CardListTableViewController: UITableViewController {
         return cell
     }
     
+    
+    // MARK: - UITableViewDelegate
+    
     override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return 80
     }
@@ -77,20 +113,15 @@ class CardListTableViewController: UITableViewController {
         }
         
         cardDetailVC.promotionDetail = creditCardList[indexPath.row].promotionDetail
+        
         self.navigationController?.pushViewController(cardDetailVC, animated: true)
         
-        // Option1
-         let cardID = creditCardList[indexPath.row].id
-        // ref?.child("Item\(cardID)/isSelected").setValue(true)
+        let cardID = creditCardList[indexPath.row].id
         
-        // Option2
-        ref?.queryOrdered(byChild: "id").queryEqual(toValue: cardID).observe(.value) { [weak self] snapshot in
-            guard let self = self,
-                  let value = snapshot.value as? [String: [String: Any]],
-                  let key = value.keys.first else { return }
-            
-            self.ref?.child("\(key)/isSelected").setValue(true)
-        }
+        updateDataToFirestoreWhenKnowPath(with: cardID)
+        // updateDataToFirestoreWhenNotKnowPath(with: cardID)
+        // updateDataToRealTimeDBWhenKnowPath(with: cardID)
+        // updateDataToRealTimeDBWhenNotKnowPath(with: cardID)
     }
     
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -99,19 +130,80 @@ class CardListTableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Option1
             let cardID = creditCardList[indexPath.row].id
             
-            ref?.child("Item\(cardID)").removeValue()
+            deleteDataToFirestoreWhenKnowPath(with: cardID)
+            // deleteDataToFirestoreWhenNotKnowPath(with: cardID)
+            // deleteDataToRealTimeDBWhenKnowPath(with: cardID)
+            // deleteDataToRealTimeDBWhenNotKnowPath(with: cardID)
+        }
+    }
+    
+    
+    private func updateDataToFirestoreWhenKnowPath(with cardID: Int) {
+        // Option1(경로를 알고 있을때)
+        firestoreDB.collection("creditCardList").document("card\(cardID)").updateData(["isSelected": true])
+        
+    }
+    
+    private func updateDataToFirestoreWhenNotKnowPath(with cardID: Int) {
+        // Option2(경로를 모를때)
+        firestoreDB.collection("creditCardList").whereField("id", isEqualTo: cardID).getDocuments { snapshot, _ in
+            guard let document = snapshot?.documents.first else {
+                print("ERROR Firesotre fetching document")
+                return
+            }
             
-            // Option2
-            // ref.queryOrdered(byChild: "id").queryEqual(toValue: cardID).observe(.value) { [weak self] snapshot in
-            //    guard let self = self,
-            //         let value = snapshot.value as? [String: [String: Any]],
-            //          let key = value.keys.first else { return }
-
-            //    self.ref.child(key).removeValue()
-            // }
+            document.reference.updateData(["isSelected" : true])
+        }
+    }
+    
+    private func updateDataToRealTimeDBWhenKnowPath(with cardID: Int) {
+        // Option1(경로를 알고 있을때)
+        reference?.child("Item\(cardID)/isSelected").setValue(true)
+    }
+    
+    private func updateDataToRealTimeDBWhenNotKnowPath(with cardID: Int) {
+        // Option2(경로를 모를때)
+        reference?.queryOrdered(byChild: "id").queryEqual(toValue: cardID).observe(.value) { [weak self] snapshot in
+            guard let self = self,
+                  let value = snapshot.value as? [String: [String: Any]],
+                  let key = value.keys.first else { return }
+            
+            self.reference?.child("\(key)/isSelected").setValue(true)
+        }
+    }
+    
+    private func deleteDataToFirestoreWhenKnowPath(with cardID: Int) {
+        // Option1(경로를 알고 있을때)
+        firestoreDB.collection("creditCardList").document("card\(cardID)").delete()
+    }
+    
+    private func deleteDataToFirestoreWhenNotKnowPath(with cardID: Int) {
+        // Option2(경로를 모를때)
+        firestoreDB.collection("creditCardList").whereField("id", isEqualTo: cardID).getDocuments { snapshot, _ in
+            guard let document = snapshot?.documents.first else {
+                print("ERROR")
+                return
+            }
+            
+            document.reference.delete()
+        }
+    }
+    
+    private func deleteDataToRealTimeDBWhenKnowPath(with cardID: Int) {
+        // Option1(경로를 알고 있을때)
+        reference?.child("Item\(cardID)").removeValue()
+    }
+    
+    private func deleteDataToRealTimeDBWhenNotKnowPath(with cardID: Int) {
+        // Option2(경로를 모를때)
+        reference?.queryOrdered(byChild: "id").queryEqual(toValue: cardID).observe(.value) { [weak self] snapshot in
+            guard let self = self,
+                  let value = snapshot.value as? [String: [String: Any]],
+                  let key = value.keys.first else { return }
+            
+            self.reference?.child(key).removeValue()
         }
     }
 }
