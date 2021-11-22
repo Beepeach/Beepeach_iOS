93539765d3c7314ae5596bfaa6eb989dcd37984c
Beepeach <eh7457303j@gmail.com>
2021-11-22 22:43:35 +0900

    Firebase Remote Config, A/B Testing를 이용한 공지사항 앱 생성

---
+++ FirebaseNotice/FirebaseNotice/ViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  FirebaseNotice
+//
+//  Created by JunHeeJo on 11/22/21.
+//
+
+import UIKit
+import FirebaseRemoteConfig
+import FirebaseAnalytics
+
+class ViewController: UIViewController {
+    var remoteConfig: RemoteConfig?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        remoteConfig = RemoteConfig.remoteConfig()
+        
+        let setting = RemoteConfigSettings()
+        // 최대한 자주 원격구성을 가져올 수 있도록 설정
+        setting.minimumFetchInterval = 0
+        
+        remoteConfig?.configSettings = setting
+        
+        remoteConfig?.setDefaults(fromPlist: "RemoteConfigDefault")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        self.getNotice()
+    }
+}
+
+extension ViewController {
+    func getNotice() {
+        guard let remoteConfig = self.remoteConfig else { return }
+        remoteConfig.fetch { [weak self] status, _ in
+            if status == .success {
+                remoteConfig.activate(completion: nil)
+            } else {
+                print("ERROR: Config not fetched")
+            }
+            
+            guard let self = self else { return }
+            
+            if !self.isNoticeHidden(remoteConfig) {
+                let notiveVC = NoticeViewController(nibName: "NoticeViewController", bundle: nil)
+                notiveVC.modalPresentationStyle = .custom
+                notiveVC.modalTransitionStyle = .crossDissolve
+                
+                let title = (remoteConfig["title"].stringValue ?? "").replacingOccurrences(of: "\\n", with: "\n")
+                let detail = (remoteConfig["detail"].stringValue ?? "").replacingOccurrences(of: "\\n", with: "\n")
+                let date = (remoteConfig["date"].stringValue ?? "").replacingOccurrences(of: "\\n", with: "\n")
+                
+                notiveVC.noticeContents = (title: title, detail: detail, date: date)
+                self.present(notiveVC, animated: true, completion: nil)
+            } else {
+                self.showEventAlert()
+            }
+        }
+    }
+    
+    func isNoticeHidden(_ remoteConfig: RemoteConfig) -> Bool {
+        return remoteConfig["isHidden"].boolValue
+    }
+}
+
+
+// ABTesting
+extension ViewController {
+    func showEventAlert() {
+        guard let remoteConfig = remoteConfig else { return }
+        
+        remoteConfig.fetch { [weak self] status, _ in
+            if status == .success {
+//                remoteConfig.activate(completion: nil)
+            } else {
+                print("Config not fetched")
+            }
+            
+            let message = remoteConfig["message"].stringValue ?? ""
+            let alertController = UIAlertController(title: "깜짝이벤트", message: message, preferredStyle: .alert)
+            let confirmAction = UIAlertAction(title: "확인", style: .default) { _ in
+                // Google Analytics가 기록하게 하기
+                Analytics.logEvent("promotionAlert", parameters: nil)
+            }
+            let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+            alertController.addAction(confirmAction)
+            alertController.addAction(cancelAction)
+            
+            self?.present(alertController, animated: true, completion: nil)
+        }
+    }
+}
