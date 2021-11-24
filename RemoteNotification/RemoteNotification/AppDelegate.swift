4ca0faf73155f0d00bda5a4a9506ff275a67e750
Beepeach <eh7457303j@gmail.com>
2021-11-24 22:08:38 +0900

    Firebase를 이용한 Remote Notification 연습

---
+++ RemoteNotification/RemoteNotification/AppDelegate.swift
@@ -0,0 +1,80 @@
+//
+//  AppDelegate.swift
+//  RemoteNotification
+//
+//  Created by JunHeeJo on 11/24/21.
+//
+
+import UIKit
+import Firebase
+import UserNotifications
+import FirebaseMessaging
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().delegate = self
+        Messaging.messaging().delegate = self
+        
+        application.registerForRemoteNotifications()
+        
+        printFCMToken()
+        
+        return true
+    }
+    
+    private func requestNotificationAuth() {
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { _, error in
+            print("ERROR, Request Notification Authorication: \(error.debugDescription)")
+        }
+    }
+    
+    private func printFCMToken() {
+        Messaging.messaging().token { token, error in
+            if let error = error {
+                print("ERROR: FCM TOKEN ERROR \(error)")
+            } else if let token = token {
+                print("FCM TOKEN: \(token)")
+            }
+        }
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
+
+// MARK: - UNUserNotificationCenterDelegate
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.list, .banner, .sound, .badge])
+    }
+}
+
+
+// MARK: - MessagingDelegate
+extension AppDelegate: MessagingDelegate {
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        guard let token = fcmToken else {
+            return
+        }
+        print("FCM TOKEN RENEW: \(token)")
+    }
+}
