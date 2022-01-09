dd130ac3d0da1a775669d69248d33a8f6f033699
Beepeach <eh7457303j@gmail.com>
2022-01-09 22:18:29 +0900

    Session을 테스트를 연습하기 위해서 프로젝트 생성

---
+++ TestSession/TestSession/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestSession
+//
+//  Created by JunHeeJo on 1/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
