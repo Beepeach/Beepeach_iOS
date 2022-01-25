4c6ba16fe7c85a3c1bfe27bc4638a2df6e7a787b
Beepeach <eh7457303j@gmail.com>
2022-01-25 16:35:28 +0900

    Feat: Storyboard를 통해서 VC에 접근하는 방법 연습
    Storyboard에 접근하는 방법과 storyboard에 만든 VC에 접근하는 방법

--- StoryboardPrac/StoryboardPrac/ViewController.swift
+++ StoryboardPrac/StoryboardPrac/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func tapModalToCode(_ sender: Any) {
+        let secondStoryboard = UIStoryboard(name: "Second", bundle: .main)
+        // let secondStoryboard = UIStoryboard(name: "Second", bundle: nil)
+        
+        let secondVC = secondStoryboard.instantiateViewController(withIdentifier: "SecondViewControlle")
+        //guard let secondVC = secondStoryboard.instantiateInitialViewController() else { return }
+        
+        present(secondVC, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
 
