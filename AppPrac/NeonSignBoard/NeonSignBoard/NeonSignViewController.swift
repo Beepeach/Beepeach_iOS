a5b026b749d81136cb34960fbb5350f3b68b6ef7
Beepeach <eh7457303j@gmail.com>
2022-02-02 21:10:03 +0900

    Feat: 네온사인어플 구현 완료
    delegate를 이용해서 데이터전환을 구현해보았습니다.
    색상 설정은 UIColorWell을 사용해 보았습니다.
    
    Resolves: #1

--- AppPrac/NeonSignBoard/NeonSignBoard/NeonSignViewController.swift
+++ AppPrac/NeonSignBoard/NeonSignBoard/NeonSignViewController.swift
@@ -8,8 +8,42 @@
 import UIKit
 
 class NeonSignViewController: UIViewController {
-
+    // MARK: @IBOutlet
+    @IBOutlet weak var textLabel: UILabel!
+    
+    // MARK: ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingVC = segue.destination as? SettingViewController else {
+            return
+        }
+        
+        configureSettingVC(settingVC)
+    }
+    
+    private func configureSettingVC(_ vc: SettingViewController) {
+        vc.delegate = self
+        
+        vc.text = textLabel.text
+        vc.textSize = textLabel.font.pointSize
+        vc.textColor = textLabel.textColor
+        vc.backgroundColor = view.backgroundColor
+    }
+}
+
+
+extension NeonSignViewController: NeonSignBoardSettingDelegate {
+    func neonSignBoard(_vc: UIViewController, didFinishSetting text: String?, with size: CGFloat, withTextColor textColor: UIColor, withBackgroundColor background: UIColor) {
+        configureTextLabel(text: text, size: size, color: textColor)
+        view.backgroundColor = background
+    }
+    
+    private func configureTextLabel(text: String?, size: CGFloat, color: UIColor) {
+        textLabel.text = text
+        textLabel.font = UIFont.systemFont(ofSize: size, weight: .regular)
+        textLabel.textColor = color
     }
 }
--- AppPrac/NeonSignBoard/NeonSignBoard/SettingViewController.swift
+++ AppPrac/NeonSignBoard/NeonSignBoard/SettingViewController.swift
@@ -7,11 +7,72 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+protocol NeonSignBoardSettingDelegate: AnyObject {
+    func neonSignBoard(_vc: UIViewController, didFinishSetting text: String?, with size: CGFloat, withTextColor textColor: UIColor, withBackgroundColor background: UIColor)
+}
 
+
+class SettingViewController: UIViewController {
+    // MARK: Properties
+    weak var delegate: NeonSignBoardSettingDelegate?
+    var text: String?
+    var textSize: CGFloat?
+    var textColor: UIColor?
+    var backgroundColor: UIColor?
+    
+    // MARK: @IBOutlet
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var textSizeSlider: UISlider!
+    @IBOutlet weak var textColorWell: UIColorWell!
+    @IBOutlet weak var backgroundColorWell: UIColorWell!
+    
+    // MARK: ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureColorWell()
 
+        self.textView.text = text
+        self.textSizeSlider.value = Float(textSize ?? 18.0)
+    }
+    
+    private func configureColorWell() {
+        configureTextColorWell()
+        configureBackgroundColorWell()
+    }
+    
+    private func configureTextColorWell() {
+        self.textColorWell.backgroundColor = .systemBackground
+        self.textColorWell.selectedColor = textColor
+        self.textColorWell.title = "Text Color"
+    }
+    
+    private func configureBackgroundColorWell() {
+        self.backgroundColorWell.backgroundColor = .systemBackground
+        self.backgroundColorWell.selectedColor = backgroundColor
+        self.backgroundColorWell.title = "Background Color"
+    }
+
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
         
+        saveSetting()
+
+        delegate?.neonSignBoard(
+            _vc: self,
+            didFinishSetting: text,
+            with: textSize ?? 18.0,
+            withTextColor: textColor ?? .black,
+            withBackgroundColor: backgroundColor ?? .white
+        )
+    }
+    
+    private func saveSetting() {
+        text = textView.text
+        textSize = CGFloat(textSizeSlider.value)
+        textColor = textColorWell.selectedColor
+        backgroundColor = backgroundColorWell.selectedColor
     }
 }
+
+
