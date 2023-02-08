1b5bfcef58cd612823f6babd2d66bb429caef1a4
Beepeach <eh7457303j@gmail.com>
2023-02-08 09:41:46 +0900

    Feat: AVKit 예제 프로젝트 생성 및 화면 구성

---
+++ Prac/AVKitPrac/AVKitPrac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AVKitPrac
+//
+//  Created by JunHeeJo on 2/8/23.
+//
+
+import UIKit
+import AVKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var thumbnailView: UIImageView!
+    
+    @IBAction func tapThumbnail(_ sender: UIButton) {
+    }
+    
+    @IBAction func downloadToDisk(_ sender: UIButton) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
